d781aabb4ca0c60036525e9f7081fcff95569dc9
SusanGicheru07 <susangicheru07@gmail.com>
2023-08-11 15:05:54 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints hexadecimal numbers
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+
+	char numbers = '0';
+	char letters = 'a';
+
+	while (numbers <= '9')
+	{
+		putchar(numbers);
+		numbers++;
+	}
+	while (letters <= 'f')
+	{
+		putchar(letters);
+		letters++;
+	}
+	putchar('\n');
+	return (0);
+
+}
