18b798ea73f65d65114afb58fa545beca82c732f
SusanGicheru07 <susangicheru07@gmail.com>
2023-08-11 15:09:56 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print numbers separated by commas & space
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+
+	int numbers = '0';
+
+	while (numbers <= '9')
+	{
+		putchar(numbers);
+		if (numbers <= '8')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		numbers++;
+	}
+	putchar('\n');
+	return (0);
+
+}
