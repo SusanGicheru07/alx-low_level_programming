aa59c3356816dbb4d86661d032fd3766aad4efea
SusanGicheru07 <susangicheru07@gmail.com>
2023-08-11 14:20:48 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Print alpha in caps and low case
+ *
+ * Return: (0)
+ *
+ */
+
+int main(void)
+{
+	char Lower = 'a';
+	char Upper = 'A';
+
+	while (Lower <= 'z')
+	{
+		putchar(Lower);
+		Lower++;
+	}
+	while (Upper <= 'Z')
+	{
+		putchar(Upper);
+		Upper++;
+	}
+	putchar('\n');
+	return (0);
+}
