d1b2abe8faae0763db54ad4070c7da38d058156b
SusanGicheru07 <susangicheru07@gmail.com>
2023-08-11 16:01:15 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,38 +1,50 @@
-#include <stdlo.h>
-#include <stdlib.h>
-#include <time.h>
-
+#include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0
+ * main - prints 4 digits separated by space and comma
+ *
+ * Return: (0)
+ *
  */
-
 int main(void)
-
 {
+	int num1, num2, num3, num4;
 
-	int p, q;
+    num1 = num2 = num3 = num4 = '0';
 
-	for (p = 0; p < 100; p++)
-	{
+    while (num1 <= '9')
+    {
+        while (num2 <= '9')
+        {
+            while (num3 <= '9')
+            {
+                while (num4 <= '9')
+                {
+                    if (((num3 + num4) > (num1 + num2) && num3 >= num1) || num1 < num3)
+                    {
+                        putchar(num1);
+                        putchar(num2);
+                        putchar(' ');
+                        putchar(num3);
+                        putchar(num4);
 
-		for (q = 0; q < 100; q++)
-		{
+                        if (!(num1 == '7' && num2 == '8' && num3 == '9' && num4 == '9'))
+                        {
+                            putchar(',');
+                            putchar(' ');
+                        }
+                    }
+                    num4++;
+                }
+                num4 = '0';
+                num3++;
+            }
+            num3 = '0';
+            num2++;
+        }
+        num2 = '0';
+        num1++;
+    }
 
-			putchar((p / 10) + 48);
-			putchar((p % 10) + 48);
-			putchar(' ');
-			putchar((q / 10) + 48);
-			putchar((q % 100 + 48);
-			if (p != 98 || q != 99) 
-			{
-				putchar(',');
-				putchar('');
-			}
-		}
-	}
+    putchar('\n');
+    return 0;
 }
-putchar('\n');
-return (0);
-
-
