31047a2bc43d6905c527a14346a2e5a47a395c9c
SusanGicheru07 <susangicheru07@gmail.com>
2023-08-11 16:23:25 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,44 +7,29 @@
  */
 int main(void)
 {
-	int num1, num2, num3, num4;
+	int i, j;
+	int a, b, c, d;
 
-    num1 = num2 = num3 = num4 = '0';
+    	for (i - 0; i < 100; i++)
+	{
+		a = i / 10;
+		b = i % 10;
 
-    while (num1 <= '9')
-    {
-        while (num2 <= '9')
-        {
-            while (num3 <= '9')
-            {
-                while (num4 <= '9')
-                {
-                    if (((num3 + num4) > (num1 + num2) && num3 >= num1) || num1 < num3)
-                    {
-                        putchar(num1);
-                        putchar(num2);
-                        putchar(' ');
-                        putchar(num3);
-                        putchar(num4);
+		for (j = 0; j < 100; j++)
+		{
+			c = j / 10;
+			d = j % 10;
 
-                        if (!(num1 == '7' && num2 == '8' && num3 == '9' && num4 == '9'))
-                        {
-                            putchar(',');
-                            putchar(' ');
-                        }
-                    }
-                    num4++;
-                }
-                num4 = '0';
-                num3++;
-            }
-            num3 = '0';
-            num2++;
-        }
-        num2 = '0';
-        num1++;
-    }
+			if (a < c || (a == c && b < d))
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
 
-    putchar('\n');
-    return 0;
+				if (!(a == 9 && b == 8))
+			}
+		}
+	}
 }
