a249cf0bd2bea3505bcbf9853dd848cc907dc3f6
SusanGicheru07 <susangicheru07@gmail.com>
2023-08-15 13:46:53 -0700

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -18,22 +18,3 @@ void print_alphabet_x10(void)
 	}
 }
 
-
-Task 3
-
-#include "main.h"
-
-/**
- * _islower - Checks for lowercase alphabet
- * @c: Character to be checked
- * Return: if character is lowercase, otherwise
- */
-
-int _islower(int c)
-{
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
-}
-
